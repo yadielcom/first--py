5a29999548cf5844c1ef5b432d1a180c982739f4
yadielcom <yadger68@gmail.com>
2022-12-21 19:59:10 +0300

    first file

---
+++ square.py
@@ -0,0 +1,111 @@
+#name = input("naem: ")
+#print("hello, " + name)
+#print(f"hii, {name}")
+
+
+                #CONDITIO
+
+#n = int(input("Number: "))
+
+#if n > 0:
+#    print("n is positive")
+#elif n < 0:
+#    print(" n is negative")
+#else:
+#    print("n is zero")
+
+                #sequences
+
+#name = "Herry"
+#print(name [1])
+
+                #for multiple name
+
+#naems = ["Herry", "Ron", "Hermione"]
+#print(naems[0])
+
+
+
+                #list
+
+
+
+#names = ["Herry", "Ron", "Hermione", "Ginny"]
+#names.append("Draco")
+#names.sort()
+#print(names)
+
+
+
+                #sets
+
+
+#s.add(1)
+#s.add(2)
+#s.add(2)
+#s.add(3)
+#s.add(4)
+#s.add(3)
+
+#s.remove(2)
+
+#print(f"the set has {len(s)} elements.")
+
+
+
+
+                        #loopes
+
+#for i in [0, 1, 2, 3, 4, 5]:
+#    print(i)
+#for i in range(6):
+#    print(i)
+
+#names = ["Herry", "Ron", "Hermione"]
+#for name in names:
+#    print(name)
+
+
+#name = "Herry"
+#for character in name:
+#    print(character)
+
+
+
+
+
+                            #dictionar
+
+#houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+#print(houses["Harry"])
+
+                            #functions
+#def square(x):
+#    return x * x
+                            # i have another file name function and it has the  definition of the requered value for under code in this case i have to link thies two files. In order to do that . . 
+
+#from function import square 
+
+#for i in range(10):
+#    print(f"the spuare of {i} is {square(i)}")
+
+
+            #OR  to import we can say
+import function
+
+print(f"The square of {i} is {function.square(i)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
